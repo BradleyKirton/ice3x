58ae206785b823a5f93d9ba31fad4f161f3ab073
Bradley Stuart Kirton <bradleykirton@gmail.com>
2018-01-20 19:14:56 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+import re
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+# Some helper functions
+def get_version():
+  PATTERN = '__version__\s+=\s+(?P<version>.*)'
+
+  with open(os.path.join(os.path.dirname(__file__), 'overlay/__init__.py'), 'r') as f:
+      match = re.search(PATTERN, f.read())
+      match_dict = match.groupdict()
+
+  return match_dict['version']
+
+
+def get_readme():
+  with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+      readme = f.read()
+
+  return readme
+
+
+requirements = [
+    'requests'
+]
+
+
+if __name__ == '__main__':
+    readme = get_readme()
+    version = get_version()
+
+    setup(
+      name='Ice3x',
+      version=version,
+      author='Bradley Stuart Kirton',
+      author_email='bradleykirton@gmail.com',
+      packages=find_packages(),
+      description='Ice3x Crypto Currency Exchanage Python API',
+      long_description=readme,
+      install_requires=requirements,
+      extras_require={'dev': ['pytest']}
+    )
