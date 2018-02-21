bb74bdf9b0d6c0ae10d24973892bacc04028ed56
Bradley Stuart Kirton <bradleykirton@gmail.com>
2018-02-21 22:44:24 +0200

    Added a snippet from the setup file of drf jwt to publish using twine.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import re
 import os
+import shutil
+import sys
 
 from setuptools import setup
 from setuptools import find_packages
@@ -13,6 +15,15 @@ def get_version():
       match_dict = match.groupdict()
 
   return match_dict['version']
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    shutil.rmtree('dist')
+    shutil.rmtree('build')
+    shutil.rmtree('ice3x.egg-info')
+    sys.exit()
 
 
 requirements = [
